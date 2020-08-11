c18cbfd7d250241e76bd974bcda7f465d11e8a1b
Danil Smenov <semenoff.da2018@yandex.ru>
2020-08-11 10:23:48 +0300

    Initial Commit

---
+++ VKOAuth2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKOAuth2
+//
+//  Created by Admin on 11.08.2020.
+//  Copyright © 2020 Danil Semenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
