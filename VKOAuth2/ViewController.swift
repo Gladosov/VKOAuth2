231c8efe2270f7d6e507c9fd8b7094af5a4d04f6
Danil Smenov <semenoff.da2018@yandex.ru>
2020-08-11 13:47:41 +0300

    AuthService

--- VKOAuth2/ViewController.swift
+++ VKOAuth2/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
 
@@ -16,7 +17,27 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func touchedLogin(button: UIButton) {
-        print("Login")
+        guard let vkUrl = URL(string: "https://oauth.vk.com/authorize?client_id=7469071&redirect_uri=https://google.com&display=mobile&response_type=token&scope=8194&revoke=1") else { return }
+        
+        let session = ASWebAuthenticationSession(url: vkUrl, callbackURLScheme: "https://google.com") { (url, error) in
+            if error != nil {
+                print(error.debugDescription)
+                return
+            }
+            print("Ok")
+        }
+        
+        session.presentationContextProvider = self
+        
+        session.start()
+        
     }
 }
 
+extension LoginViewController: ASWebAuthenticationPresentationContextProviding {
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return view.window!
+    }
+    
+    
+}
