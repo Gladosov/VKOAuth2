f7b8902f182f8acd2de3921d786d91d30904f541
Danil Smenov <semenoff.da2018@yandex.ru>
2020-08-11 10:35:52 +0300

    Initial

--- VKOAuth2/ViewController.swift
+++ VKOAuth2/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  VKOAuth2
 //
 //  Created by Admin on 11.08.2020.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchedLogin(button: UIButton) {
+        print("Login")
+    }
 }
 
