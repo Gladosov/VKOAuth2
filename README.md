5d486f062414a7acdd21a18b1654c1265191a97c
Danil Smenov <semenoff.da2018@yandex.ru>
2020-09-24 12:37:36 +0300

    UIProgressView was added to WebView

--- README.md
+++ README.md
@@ -12,5 +12,4 @@ A client for VK created for educational purposes, learning new technologies, and
 * Display tabbar for authorized user (Friends, Wall, Photos)
 
 <img src="img/Simulator Screen Shot - iPhone 11 Pro Max - 2020-09-24 at 11.57.58.png" width="400" />
-<img src="img/Simulator Screen Shot - iPhone 11 Pro Max - 2020-09-18 at 12.25.33.png" width="400" />
 <img src="img/Simulator Screen Shot - iPhone 11 Pro Max - 2020-09-24 at 11.58.22.png" width="400" />
--- VKOAuth2/Modules/Web/WebViewController.swift
+++ VKOAuth2/Modules/Web/WebViewController.swift
@@ -22,6 +22,14 @@ final class WebViewController: UIViewController {
         webView.translatesAutoresizingMaskIntoConstraints = false
         return webView
     }()
+    
+    lazy var progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.trackTintColor = .systemFill
+        progressView.tintColor = .systemBlue
+        return progressView
+    }()
 
     // MARK: - Lifecycle -
 
@@ -30,22 +38,38 @@ final class WebViewController: UIViewController {
         
         setupViews()
         setupLayout()
+        setupBinding()
     }
     
     func setupViews() {
         webView.navigationDelegate = self
         view.addSubview(webView)
+        view.addSubview(progressView)
     }
     
     func setupLayout() {
         NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.topAnchor.constraint(equalTo: progressView.bottomAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
     
+    func setupBinding() {
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            let progress = Float(webView.estimatedProgress)
+            self.progressView.setProgress(progress, animated: true)
+        }
+    }
 }
 
 // MARK: - Extensions -
@@ -61,5 +85,6 @@ extension WebViewController: WebViewInterface {
 extension WebViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         presenter.didFinishLoadWebPage(with: webView.url)
+        progressView.isHidden = true
     }
 }
